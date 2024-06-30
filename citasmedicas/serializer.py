361ffdc84aa839c9f67ece700af3536b6d2bb7f9
Edwin <66150041+TacuriErasEdwin@users.noreply.github.com>
2024-06-30 06:32:24 -0500

    Create serializer.py

---
+++ citasmedicas/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework import Serializer
+from ,models import Doctores, Sintomas, Cita
+
+class DoctoresSerializer (serializer.ModelsSerializer);
+	class Meta:
+		model = Doctores
+		#fields:('id','nombre')
+		field = '_all_'
+
+class SintomasSerializer (serializer.ModelsSerializer);
+	class Meta:
+		model = Sintomas
+		field = '_all_'
+
+class CitaSerializer (serializer.ModelsSerializer);
+	class Meta:
+		model = Cita
+		field = '_all_'
