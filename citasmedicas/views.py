d9fa0ef87dcb15f2acaef0284454113b6e093016
Edwin <66150041+TacuriErasEdwin@users.noreply.github.com>
2024-06-30 14:43:49 -0500

    Update views.py

--- citasmedicas/views.py
+++ citasmedicas/views.py
@@ -1,19 +1,19 @@
-form django.shorcuts import render
+from django.shortcuts import render
 
 from rest_framework import viewsets
-from .serializer import DoctoresSerializer, SintomasSerializer, CitaSerializer
+from .serializer import DoctoresSerializer, SintomasSerializer, CitaSerializer 
 from .models import Doctores, Sintomas, Cita
+# Create your views here.
 
-#Create your views here.
 
-class Doctoresview(viewsets.ModelViewSet);
+class Doctoresview(viewsets.ModelViewSet):
 	serializer_class = DoctoresSerializer
 	queryset = Doctores.objects.all()
 
-class Sintomasview(viewsets.ModelViewSet);
+class Sintomasview(viewsets.ModelViewSet):
 	serializer_class = SintomasSerializer
 	queryset = Sintomas.objects.all()
 
-class Citasview(viewsets.ModelViewSet);
-	serializer_class = CitasSerializer
-	queryset = Citas.objects.all()
+class Citaview(viewsets.ModelViewSet):
+	serializer_class = CitaSerializer
+	queryset = Cita.objects.all()
