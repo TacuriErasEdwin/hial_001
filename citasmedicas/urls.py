f5edfc85b2866a7805e107b1b3d707575cbda247
Edwin <66150041+TacuriErasEdwin@users.noreply.github.com>
2024-06-30 17:08:44 -0500

    Update urls.py

--- citasmedicas/urls.py
+++ citasmedicas/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
-from rest_framework.documentation import include_docs_urls
-
-router=routers.DefaultRouter()
-router.register(r'doctores', views.DoctoresView, 'doctores')
-router.register(r'sintomas', views.SintomasView, 'sintomas')
-router.register(r'citas', views.CitaView, 'citas')
 
 urlpatterns = [
-    path('home/',views.principal ),
-    path('about/',views.about ),
-    path('doctores/',views.doctores ),
-    path('api/',include(router.urls) ),
-    path('docsapi/',include_docs_urls(title='Citas Medicas API V2') ),
-]
+    path('api/v1/', include(router.urls)),   # atravez de esta ryta vamos a llamar a los elementos
+    ]
+
+
+/// 
+			//instalamos en la consola:
+pip install coreapi  	//instalamos toda la documentacion instlar automaticamente del api
