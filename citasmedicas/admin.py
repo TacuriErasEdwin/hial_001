2caca85d24663b85a1bc4d475c6f3326d90f4e6a
Edwin <66150041+TacuriErasEdwin@users.noreply.github.com>
2024-07-07 22:50:06 -0500

    Update admin.py

--- citasmedicas/admin.py
+++ citasmedicas/admin.py
@@ -1,7 +1,13 @@
-# Register your models here.
 from django.contrib import admin
-from .models import Doctores, Sintomas, Cita
+from .models import Paciente, Doctor, Especialidad, DocEspe, Cita, AtencionMedica, HistoriaMedica, Farmacia
 
-admin.site.register(Doctores)
-admin.site.register(Sintomas)
+# Register your models here.
+
+admin.site.register(Paciente)
+admin.site.register(Doctor)
+admin.site.register(Especialidad)
+admin.site.register(DocEspe)
 admin.site.register(Cita)
+admin.site.register(AtencionMedica)
+admin.site.register(HistoriaMedica)
+admin.site.register(Farmacia)
