587ec3d97abaa10c39076f3edea2936f14c70da2
Edwin <66150041+TacuriErasEdwin@users.noreply.github.com>
2024-12-17 04:31:18 -0500

    Update views.py

--- citasmedicas/views.py
+++ citasmedicas/views.py
@@ -4,11 +4,17 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializer import PacienteSerializer, DoctorSerializer, EspecialidadSerializer, DocEspeSerializer, CitaSerializer, AtencionMedicaSerializer, HistoriaMedicaSerializer, FarmaciaSerializer
 from .models import Paciente, Doctor, Especialidad, DocEspe, Cita, AtencionMedica, HistoriaMedica, Farmacia
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout 
+from .forms import SignupForm, LoginForm, NuevoDoctorModelForm, NuevoPacienteModelForm, NuevoEspecialidadodelForm,NuevoCitaodelForm, NuevoAtencionModelForm, NuevoHistoriaModelForm, NuevoFarmaciaModelForm
 
 # Llamar a los templates
 def index(request):                                          #nombre será el que se llame en la urls views.index 
-    title='Portal de citas medicas'                          #una variable
-    return render(request, 'home.html', {'title':title})     #llamo a la variable
+    
+    return render(request, 'home.html')     #llamo a la variable
+    doctor = Doctor.objects.all()
+def about(request):
+    return render(request, 'about.html')   
 
 def paciente(request):
     paciente = Paciente.objects.all()
@@ -16,7 +22,7 @@ def paciente(request):
 
 def doctor(request):
     doctor = Doctor.objects.all()
-    return render(request, 'doctor.html',{'doctor':doctor})
+    return render(request, 'doctor.html',{'doctor':doctor})   #voy a enviar una variable a doctores html
 
 def especialidad(request):
     especialidad = Especialidad.objects.all()
@@ -41,6 +47,74 @@ def historiaMedica(request):
 def farmacia(request):
     farmacia = Farmacia.objects.all()
     return render(request, 'farmacia.html',{'farmacia':farmacia})
+
+
+
+# signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+# login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)    
+                return redirect('/home/')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+# logout page
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+#llamar a los forms
+def NuevoDoc(request):
+    form = NuevoDoctorModelForm()
+    if request.method == 'POST':
+        form = NuevoDoctorModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home/')
+    else:
+        form = NuevoDoctorModelForm()
+    return render(request, 'nuevodoctor.html', {'form': form})
+
+def NuevoPa(request):
+    form = NuevoPacienteModelForm()
+    if request.method == 'POST':
+        form = NuevoPacienteModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home/')
+    else:
+        form = NuevoPacienteModelForm()
+    return render(request, 'nuevopaciente.html', {'form': form})
+
+def NuevoEspe(request):
+    form = NuevoEspecialidadodelForm()
+    if request.method == 'POST':
+        form = NuevoEspecialidadodelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/home/')
+    else:
+        form = NuevoEspecialidadodelForm()
+    return render(request, 'nuevoespecialidad.html', {'form': form})
+
 
 # Create your views here.
 #vistas para el serializer
